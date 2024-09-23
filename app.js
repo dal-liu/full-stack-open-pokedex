63349b7e5e50da0856dcf108f56ed7aeeedd133e
Daniel <daniel.albert.liu@gmail.com>
2024-09-23 12:29:52 -0500

    Completed 11.12

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
