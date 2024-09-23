0f47e07ee7022a114021c60720027a79fd335d7a
Daniel <daniel.albert.liu@gmail.com>
2024-09-23 13:19:17 -0500

    Successful deploy

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
-})
-
-app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw ('error...  ')
-  res.send('ok')
 })
